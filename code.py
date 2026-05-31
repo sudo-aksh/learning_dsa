73d17e4022b80c7de11ddd43a5498338ddb8f8ba
buggu <aakashwankhade87@gmail.com>
2026-05-31 22:09:08 +0530

    made the existing into function for adding more fibo_term functions

--- code.py
+++ code.py
@@ -1,22 +1,27 @@
 # for print the nth fibbonacci number 
 # using loops first 
+def fibo_term_1(n):
+    a = 0
+    b = 1
 
-a = 0
-b = 1
+   
+    if n <0:
+        print("enter only positive numbers.")
+    elif n == 1:
+        print(0)
+    elif n == 2:
+        print(1)
+    else:
 
-n = int(input("which nth fibo num you want to print:"))
+        for i in range(n-2):
+            new = a+b
+            a = b
+            b = new
 
-if n <0:
-    print("enter only positive numbers.")
-elif n == 1:
-    print(0)
-elif n == 2:
-    print(1)
-else:
+        print(new)
 
-    for i in range(n-2):
-        new = a+b
-        a = b
-        b = new
 
-    print(new)
+# using recursion
+
+def fibo_term_2(n):
+    
